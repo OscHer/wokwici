372e3276cf5bc63124df6b3a7d7245641b6d87d9
Oscar <oscarherediallorens@gmail.com>
2025-04-25 09:58:07 +0200

    🛠 Fix final: eliminados inputs conflictivos

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,12 +1,12 @@
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
-  exit(0);
+}
+
+void loop() {
+
 }
